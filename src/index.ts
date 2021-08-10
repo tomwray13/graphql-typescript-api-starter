686e0d2ec873406046e15bcfb0b2dd6ec33132ba
Tom Ray <tomwray13@gmail.com>
2021-08-10 11:15:21 +0100

    Initial Express GraphQL server configurations

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { createServer } from "http";
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const startServer = async () => {
+  const app = express();
+  const httpServer = createServer(app);
+
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      hello: () => `Hello world!`
+    }
+  };
+
+  const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+  await apolloServer.start()
+
+  apolloServer.applyMiddleware({ app, path: `/api` })
+
+  httpServer.listen({ port: process.env.PORT || 4000 }, () =>
+    console.log(`Server listening on localhost:4000${apolloServer.graphqlPath}`)
+  )
+}
+
+startServer()
